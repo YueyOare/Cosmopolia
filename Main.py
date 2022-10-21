f868a375762e4c42a9a98c9cca8df855bb3d01d8
Lianasy <115954430+Lianasy@users.noreply.github.com>
2022-10-21 17:19:36 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -22,7 +22,7 @@ class Cosmopolia():
             Players[i+Amount_of_Humans](id = (i+Amount_of_Humans))
 
     def Randomaise_first_player(): #генеруємо першого гравця, що здійснює хід
-        first_player = randint(0, Amount_of_Players - 1)
+        first_player = Players[randint(0, Amount_of_Players - 1)]
 
     def Randomaise_dice(): #Кидаємo кубик
         Number_of_sides_of_cube = 12
@@ -37,7 +37,17 @@ class Cosmopolia():
             if(Player.get_move_main(Current_Player) != 3): #Если в тюрьме меньше трех ходов...
                 action = int(input("Enter variant of action in prison: 1 - заплатити, 2 - сідіти далі, 3 - збігти")) #Действие игрока в тюрьме
         result = Prison(action, Current_Player)
+
     def Player_cube(Current_Player): #действие игрока после броска кубика
         dice = int(Randomaise_dice()) #Кидаем кубик
         Player.set_current_field(Current_Player, dice) #находим новую позицию игрока
-        Field.event(Current_Player) #Событие с игроком на этой позиции
+        Map.array_Fields[Current_Player.get_current_field()].event(Current_Player) #Событие с игроком на этой позиции
+Cosmopolia.Create_Players() #Створюємо гравців
+Current_Player = Cosmopolia.Randomaise_first_player() #генеруємо першого гравця
+while(True): 
+    for i in range(Cosmopolia.Amount_of_Players(Current_Player)): #проходимо по циклу гравців
+        Cosmopolia.Before_turn(Current_Player) #Дії до хода гравця
+        Cosmopolia.Player_cube(Current_Player) #Дії під час хода гравця
+        if (i!=(Cosmopolia.Amount_of_Players(Current_Player) - 1)): #Умова, що це не останній ігрок в масиві
+            Current_Player = Cosmopolia.Players[Cosmopolia.Players.index(Current_Player + 1)] #переходимо до наступного гравця
+    Current_Player = Cosmopolia.Players[0] #після проходження масива повертаємося до першого гравця в масиві
